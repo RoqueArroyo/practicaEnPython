e7737f673329b8ed232583f473470eb39a2781d1
RoqueArroyo <rarroyov1800@alumno.ipn.mx>
2024-01-30 14:36:50 -0600

    cambio a bank_of_codedex y prueba

--- bank_of_codedex.py
+++ bank_of_codedex.py
@@ -6,4 +6,4 @@ while pin != 1234:
   pin = int(input('Incorrect PIN. Enter your PIN again: '))
 
 if pin == 1234:
-  print('PIN accepted!')
+  print('PIN accepted!') 
---
+++ prueba_modificacion.py
@@ -0,0 +1,3 @@
+# Solo escribi este codigo para probar la modificacion de mi repositorio
+for i in range(90): 
+    print('Hola putos!!!!')
